92e876921836e3f4631b3a8d6d13cc068fd8279b
MKS-students <mks.students@hackreactor.com>
2016-04-01 17:55:58 -0500

    Add structure to profile page, get user data for profile

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,5 +1,18 @@
 angular.module("jamz.profile", [])
 
-  .controller("ProfileCtrl", function ($scope) {
-    $scope.user = {}
+  .controller("ProfileCtrl", function ($scope, Profile) {
+
+    $scope.getProfile = function () {
+      console.log("HEY!!")
+      Profile.getProfile()
+      .then(function (data) {
+        console.log("I Look like this", data[0])
+        $scope.profile = data[0];
+      })
+      .catch(function (err) {
+        console.error(err)
+      })
+    }
+
+    $scope.getProfile();
   });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,7 +3,7 @@ angular.module("jamz.services", [])
   .factory('Sessions', function($http){
 
     var Sessions = [];
-
+    // TODO: add comments
     var getSessions = function () {
       return $http({
         method: 'GET',
@@ -54,3 +54,33 @@ angular.module("jamz.services", [])
 
     }
   })
+
+  .factory('Profile', function ($http) {
+
+    var Profile = []
+
+    var getProfile = function () {
+      console.log("Trying boss!!")
+      return $http({
+        method: 'GET',
+        url: 'sampleUserData'
+      })
+      .then(function (data) {
+        return data.data
+      })
+      .catch(function (err) {
+        console.error(err)
+      })
+    }
+
+    return {
+      getProfile: getProfile
+    }
+
+  })
+
+
+
+
+
+
