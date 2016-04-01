dd737b3a8b625bdb5b2b302b4522b86e1f109dfa
MKS-students <mks.students@hackreactor.com>
2016-04-01 17:44:55 -0500

    Setup data for creating a session

--- client/app/dash/dash-create.js
+++ client/app/dash/dash-create.js
@@ -1,5 +1,36 @@
 angular.module('jamz.dash-create', [])
 
-  .controller('CreateCtrl', function($scope, Dash){
+  .controller('CreateCtrl', function($scope, Dash) {
+    $scope.instrumentsNeeded = [];
+    $scope.genres = [];
+
+    $scope.createSession = function() {
+      var check = ("Are you sure you want to create this session?");
+
+      if (check) {
+        var genres = $scope.genres
+          .split(',')
+          .map(function(val){ return val.trim(); })
+        var instruments = $scope.instruments
+          .split(',')
+          .map(function(val){ return val.trim(); })
+
+        var session = {
+          title: $scope.title,
+          description: $scope.description,
+          genres: genres,
+          paidAmount: $scope.amount,
+          experience: $scope.experience,
+          instrumentsNeeded: instruments,
+          area: $scope.city,
+          location: $scope.location,
+          date: $scope.date,
+          time: $scope.time
+        };
+
+        console.log("session to be created:", session);
+
+      }
+    }
 
   })
