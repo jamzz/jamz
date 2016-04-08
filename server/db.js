4a443df970cb94dfc603cd43a935ad8039444ebd
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-04-08 00:04:22 -0500

    fix pull merge conflict

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 //configure knex
 var configEnvironment = require('./config/environment');
 var config = require('../knexfile');
-var knex = require('knex')(config["development"]);
+var knex = require('knex')(config.development);
 
 module.exports = knex;
